f5fcd41b8388c59e5ea70e282352d240cebc77e6
sn1ka <67114747+sn1ka@users.noreply.github.com>
2020-07-01 22:11:33 +0300

    Update davet.py

--- davet/davet.py
+++ davet/davet.py
@@ -8,5 +8,9 @@ class Mycog1(commands.Cog):
 
     @commands.command()
     async def sure(self, ctx):
-        x = datetime.datetime(2020, 5, 17)
+        x = datetime.datetime(2020, 6, 21, 19, 35, 0)
+        count_hours, rem = divmod(difference.seconds, 86400)
+        count_minutes, count_seconds = divmod(rem, 60)
         await ctx.send(x)
+    if difference.days == 0 and count_hours == 0 and count_minutes == 0 and count_seconds == 0:
+        await ctx.send("bitti")
