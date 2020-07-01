061cccb1b8812f5eaaa5e03e604b96187ebcf1bf
sn1ka <67114747+sn1ka@users.noreply.github.com>
2020-07-01 22:02:15 +0300

    Update davet.py

--- davet/davet.py
+++ davet/davet.py
@@ -7,14 +7,6 @@ class Mycog1(commands.Cog):
 
 
     @commands.command()
-    async def countdown(ctx,stop):
-        while True:
-            difference = stop - datetime.datetime
-            count_hours, rem = divmod(difference.seconds, 86400)
-            count_minutes, count_seconds = divmod(rem, 60)
-        if difference.days == 0 and count_hours == 0 and count_minutes == 0 and count_seconds == 0:
-            await ctx.send("Etkinlik Bitti!")
-        await ctx.send(difference.days)
-        await asyncio.sleep(1)
-
-end_time = datetime.datetime(2020, 6, 21, 19, 35, 0)
+    async def ping(ctx):
+        x = datetime.datetime(2020, 5, 17)
+        await ctx.send(x)
