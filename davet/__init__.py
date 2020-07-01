c9a4fae08fcaa00d396cd553aefb31d4078eee2f
sn1ka <67114747+sn1ka@users.noreply.github.com>
2020-07-01 21:07:43 +0300

    Add files via upload

---
+++ davet/__init__.py
@@ -0,0 +1,4 @@
+from .davet import Mycog1
+
+def setup(bot):
+    bot.add_cog(Mycog1())
---
+++ davet/davet.py
@@ -0,0 +1,23 @@
+
+from redbot.core import commands
+import discord
+
+class Mycog1(commands.Cog):
+    """My custom cog"""
+
+
+    @commands.command()
+    async def davet(self, ctx):
+        """Ganesh botu sunucuna davet etmek için aşağıdaki linkleri kullanabilirsin."""
+        # Your code will go here
+        e = discord.Embed() 
+        e.title = "<Davet>"
+        e.description = "Ganesh’i sunucunuzda görmekten mutluluk duyarız! **!yardım** komutu ile komutları görüntüleyebilirsiniz.**Ya da isterseniz aşağıdaki linki kullanabilirsiniz**" 
+        e.colour = discord.Color.red()
+        await ctx.author.send(embed=e) # send it
+
+        e2 = discord.Embed() 
+        e2.title = "<Davet>"
+        e2.description = "Ganesh’i sunucunuzda görmekten mutluluk duyarız! **!yardım** komutu ile komutları görüntüleyebilirsiniz." 
+        e2.colour = discord.Color.red()
+        await ctx.author.send(embed=e2) # send it
